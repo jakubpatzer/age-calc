e2257e2f6649df2efdcb0792ed615c1a04adcd5b
jakubpatzer <jakub.patzer@protonmail.com>
2024-11-15 10:14:55 +0100

    Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Poppins } from 'next/font/google';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700', '800'], // Specify weights
+  style: ['italic', 'normal'], // Include italic styles
+  display: 'swap', // Optional for better font loading
 });
 
 export const metadata: Metadata = {
@@ -26,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,20 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], // Define Poppins as a custom font
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          Purple: 'hsl(259, 100%, 65%)',
+          LightRed: 'hsl(0, 100%, 67%)',
+        },
+        neutral: {
+          OffWhite: 'hsl(0, 0%, 94%)',
+          LightGrey: 'hsl(0, 0%, 86%)',
+          SmokeyGrey: 'hsl(0, 1%, 44%)',
+          OffBlack: 'hsl(0, 0%, 8%)',
+        }
       },
     },
   },
